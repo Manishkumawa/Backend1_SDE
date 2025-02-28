2e380ff20e5dfac01c21759ad21aff26cc5ae4cf
manish kumawat <88154535+Manishkumawa@users.noreply.github.com>
2025-02-28 06:16:12 +0530

    Update settings.py

--- imageProject/imageProject/settings.py
+++ imageProject/imageProject/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
